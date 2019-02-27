84ffe430d6f3dbaaed0698323736891179de8d4f
ylltron <yllisahiti@gmail.com>
2019-02-27 15:30:29 +0100

    Added the ability to add new books to authors

--- dal/repositories/book-authors-repository.js
+++ dal/repositories/book-authors-repository.js
@@ -1,5 +1,6 @@
 
 const BookAuthor = require("../models/book_author_model").BookAuthor
+const Author = require("../models/author_model").Author
 
 
 exports.delete = function(bookAuthor) {
@@ -27,6 +28,41 @@ exports.delete = function(bookAuthor) {
                 }
                 else {
                     setTimeout(function() { throw error; });
+                }
+            }
+            return reject(error.errors)
+        })
+    })
+}
+
+exports.create = function(bookAuthor) {
+    return new Promise(function(resolve, reject) {
+
+        BookAuthor.create({
+                bookISBN: bookAuthor.bookISBN,
+                authorId: bookAuthor.authorId
+        }).then((result)=> {
+            if (result) {
+                resolve()
+            }
+            else {
+                const errors = [
+                    {message: "Could not unlink author from book."}
+                ]
+                reject(errors)
+            }
+        }).catch((error) => {
+            if (error.errors == null || error.errors.length == 0) {
+                if (error.message) {
+                    reject([error.message])
+                }
+                else {
+                    setTimeout(function() { throw error; });
+                }
+            }
+            for (let i = 0; i < error.errors.length; i++) {
+                if (error.errors[i].message == "PRIMARY must be unique") {
+                    error.errors[i].message = "This connection already exists."
                 }
             }
             return reject(error.errors)
