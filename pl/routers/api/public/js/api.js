e6fd94f77700e7977bc3e8f0005f003262561f59
Berkay Torun <tobe18ut@student.ju.se>
2019-03-26 16:56:02 +0100

    Adden API pagination and stuff knows bug not firstpage lastpage and

--- pl/routers/api/public/js/api.js
+++ pl/routers/api/public/js/api.js
@@ -1,70 +1,88 @@
 
-function startScript() {
+function startScript(page) {
 
     var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:8080/api/books/");
+
+
+    let currentPage = "currentPage=1"
+    if (page) {
+        currentPage = "currentPage=" + page
+    }
+
+    request.open("GET", "http://localhost:8080/api/books?" + currentPage);
 
     request.addEventListener("load", function () {
         if (request.status === 200) {
 
             let jsonObj = JSON.parse(request.responseText)
 
-            console.log(jsonObj)
-
-            var txt = document.getElementById("entirepage").innerHTML
-            const ourURL = "http://localhost:8080/books/"
-            for (let i = 0; i < jsonObj.pages.pageList.length; i++) {
-                txt +=  '<div id="wrapper">' +
-                        '   <div id="book_info">' +
-                        '        <a href=' + ourURL + jsonObj.books[i].ISBN + '/>' +
-                        '            <div>ISBN: ' + jsonObj.books[i].ISBN + '</div>' +
-                        '            <div>Title: ' + jsonObj.books[i].title + '</div>' +
-                        '            <div>publicationYear: ' + jsonObj.books[i].publicationYear + '</div>' +
-                        '            <div>Pages: ' + jsonObj.books[i].pages + '</div>' +
-                        '        </a>' +
-                        '   </div>' +
-                        '</div>';
-            }
-            document.getElementById("entirepage").innerHTML = txt
-            /*        
-        var myvar = ''+
-        '        <a href="/books/{{this.ISBN}}">'+
-        '            <div>ISBN: '+jsonObj.books[i].ISBN+'</div>'+
-        '            <div>Title: '+jsonObj.books[i].title+'</div>'+
-        '            <div>publicationYear: '+jsonObj.books[i].publicationYear+'</div>'+
-        '            <div>Pages: '+jsonObj.books[i].pages+'</div>'+
-        '        </a>'
-        
-        '        {{#if this.authors}}'+
-        '            <div>'+
-        '                <div>'+
-        '                    Authors: '+
-        '                </div>'+
-        '                <hr>'+
-        '                {{#each this.authors}}'+
-        '                    <a href="/authors/{{this.Id}}">'+
-        '                        <div>'+
-        '                            First Name: {{this.firstName}}<br>'+
-        '                            Last Name: {{this.lastName}}<br>'+
-        '                        </div>'+
-        '                    </a>'+
-        '                    <hr>'+
-        '                {{/each}}'+
-        '        {{/if}}';
-        	
-        
-                      
-                 
-        */
+            initiateBook(jsonObj)
+            initiatePaginatio(jsonObj)
 
         }
     });
-    request.send();
+    request.send("{hadsf: oiauffds}");
 }
 
-function runMyFunction(){
+function initiatePaginatio(jsonObj) {
 
+    let txt = document.getElementById("pagination").innerHTML
+    txt += '<ul class="pagination justify-content-center"> '
+    for (let i = 0; i < jsonObj.pages.pageList.length; i++) {
+        const page = jsonObj.pages.pageList[i]
+        let pageValue = page.value;
+        let isCurrent = ""
+        let anchorTag = "p"
+        if (page.isCurrent) {
+            isCurrent = "id=\"pageCurrent\""
+        }
+        let searchString = ""
+
+        let classBundle = "pageNumber"
+        if (jsonObj.searchString) {
+            searchString = "&searchString=" + jsonObj.searchString
+        }
+        if (pageValue == "...") {
+            classBundle = "isDisabled"
+            anchorTag = "p"
+        }
+
+        txt += ' <li class="' + classBundle + '" ' + isCurrent + '">' +
+            '    <' + anchorTag + ' href="/books?currentPage=' + pageValue + ' " ' + searchString + '" onclick=fn('+pageValue+') > ' + pageValue + '</' + anchorTag + '>' +
+            '    </li>';
+    }
+    txt += ' </ul>       '
+    document.getElementById("pagination").innerHTML = txt
 }
 
 
+function initiateBook(jsonObj) {
+    console.log(jsonObj)
+    document.getElementById("entirepage").innerHTML ='        <div id="pagination">' +
+                                                    '' +
+                                                    '        </div>' +
+                                                    '';
+
+
+    let txt = document.getElementById("entirepage").innerHTML
+    const ourURL = "http://localhost:8080/books/"
+    for (let i = 0; i < jsonObj.books.length; i++) {
+        txt += '<div id="wrapper">' +
+            '   <div id="book_info">' +
+            '        <a href=' + ourURL + jsonObj.books[i].ISBN + '/>' +
+            '            <div>ISBN: ' + jsonObj.books[i].ISBN + '</div>' +
+            '            <div>Title: ' + jsonObj.books[i].title + '</div>' +
+            '            <div>publicationYear: ' + jsonObj.books[i].publicationYear + '</div>' +
+            '            <div>Pages: ' + jsonObj.books[i].pages + '</div>' +
+            '        </a>' +
+            '   </div>' +
+            '</div>';
+    }
+    document.getElementById("entirepage").innerHTML = txt
+
+}
+function fn(event) {
+
+    startScript(event)
+}
 startScript()
